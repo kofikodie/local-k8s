948f9af31c22aadcf0bb687cfcde77ae50380ab4
kody <k.kodieaddo@gmail.com>
2022-12-30 12:54:41 +0100

    fix: addons not finding cluster error

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -82,4 +82,8 @@ resource "aws_eks_addon" "addons" {
   addon_name        = each.value.name
   addon_version     = each.value.version
   resolve_conflicts = "OVERWRITE"
+
+  depends_on = [
+    aws_eks_cluster.demo,
+  ]
 }
