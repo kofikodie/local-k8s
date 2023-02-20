53d32cf4844e97f511328f9bf802570f0462bbb1
kody <k.kodieaddo@gmail.com>
2023-02-20 12:29:01 +0100

    feat: dynamo db table

---
+++ dynamo-db.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "db" {
+    name           = "terraform-dynamodb-demo"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "id"
+    range_key      = "name"
+    read_capacity  = 1
+    write_capacity = 1
+    attribute {
+        name = "id"
+        type = "S"
+    }
+    attribute {
+        name = "name"
+        type = "S"
+    }
+    tags = {
+        Name = "terraform-dynamodb-demo"
+    }
+}
