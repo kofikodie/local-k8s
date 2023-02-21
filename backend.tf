47bbde5c9b92eb82d5c24fea48d16d1f5c5c2554
kody <k.kodieaddo@gmail.com>
2023-02-21 15:22:43 +0100

    feat: add ci jobs for terraform plan, fmt & apply

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  cloud {
-    organization = "dragon-ws"
-
-    workspaces {
-      name = "kubernetes"
-    }
-  }
-}
---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud {
+    organization = "dragon-ws"
+
+    workspaces {
+      name = "gh-action-local-k8s"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
