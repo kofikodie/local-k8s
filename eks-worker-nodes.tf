8fe71f0348570a0ed2d12d908170598f12efdee3
kody <k.kodieaddo@gmail.com>
2023-02-22 20:18:27 +0100

    feat: add cw policy for eks node

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -73,10 +73,63 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
   role       = aws_iam_role.demo-node.name
 }
 
-resource "aws_iam_role_policy_attachment" "cloudwatch_eks_logs_metrics_policy" {
+resource "aws_iam_policy" "cloudwatch_logs_metrics_policy" {
+  name = "cloudwatch-logs-metrics-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowReadingMetricsFromCloudWatch"
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:DescribeAlarmsForMetric",
+          "cloudwatch:DescribeAlarmHistory",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetInsightRuleReport",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowReadingLogsFromCloudWatch"
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogGroups",
+          "logs:GetLogGroupFields",
+          "logs:StartQuery",
+          "logs:StopQuery",
+          "logs:GetQueryResults",
+          "logs:GetLogEvents",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowReadingTagsInstancesRegionsFromEC2"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeTags",
+          "ec2:DescribeInstances",
+          "ec2:DescribeRegions",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid      = "AllowReadingResourcesForTags"
+        Effect   = "Allow"
+        Action   = "tag:GetResources"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_readonly_attachment" {
   policy_arn = aws_iam_policy.cloudwatch_logs_metrics_policy.arn
   role       = aws_iam_role.demo-node.name
 }
+
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
